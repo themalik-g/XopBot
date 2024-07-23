771dfc5f63bd10a13892b648dbbed778d4ab98bb
Astro (Daniel) <danielfx011@gmail.com>
2024-07-23 10:26:34 +0100

    Update _menu.js

--- plugins/_menu.js
+++ plugins/_menu.js
@@ -28,9 +28,17 @@ bot(
   type: 'user',
  },
  async (message, match) => {
-  let buttonMessage = {
+  let data = {
    jid: message.jid,
-   button: [{ type: 'reply', params: { display_text: 'SUPPORT', id: '#support' } }],
+   button: [
+    { type: 'list', params: { title: 'Button 1', sections: [{ title: 'Button 1', rows: [{ header: 'title', title: 'Button 1', description: 'Description 1', id: '#menu' }] }] } },
+    { type: 'reply', params: { display_text: 'MENU', id: '#menu' } },
+    { type: 'url', params: { display_text: 'Neeraj-x0', url: 'https://www.neerajx0.xyz/', merchant_url: 'https://www.neerajx0.xyz/' } },
+    { type: 'address', params: { display_text: 'Address', id: 'message' } },
+    { type: 'location', params: {} },
+    { type: 'copy', params: { display_text: 'copy', id: '123456789', copy_code: 'message' } },
+    { type: 'call', params: { display_text: 'Call', phone_number: '123456789' } },
+   ],
    header: { title: 'X-Asena', subtitle: 'WhatsApp Bot', hasMediaAttachment: false },
    footer: { text: 'Interactive Native Flow Message' },
    body: { text: 'Interactive Message' },
@@ -86,7 +94,7 @@ Description: ${command.desc}\`\`\``)
 
    menuHeader += `\n`
    menuHeader += `ᴢᴇɴᴏɴ-ʙᴏᴛ`
-   return await message.sendMessage(message.jid, menuHeader, buttonMessage, {}, 'interactive')
+   return await message.sendMessage(message.jid, menuHeader, data, {}, 'interactive')
   }
  }
 )
